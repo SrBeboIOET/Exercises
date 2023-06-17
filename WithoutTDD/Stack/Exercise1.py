19131d0fbc73bdd04d0099ada2a9ab4a957e18d9
fabricio <fabricio.simbana@ioet.com>
2023-06-17 18:53:37 -0500

    Add stack's exercises

---
+++ WithoutTDD/Stack/Exercise1.py
@@ -0,0 +1,20 @@
+string = input("Enter a string of characteres: ")
+flag = True
+
+def balance(string):
+    stack = []
+    balances  = {'(': ')', '[': ']', '{': '}'}
+
+    for special_character in string: 
+        if special_character in balances.keys(): 
+            stack.append(special_character) 
+        if special_character in balances.values(): 
+            if not stack or balances[stack.pop()] != special_character: 
+                return False
+
+    return len(stack) == 0
+
+if balance(string):
+    print("There is a balance")
+if not balance(string):
+    print("There is not a balance")
---
+++ WithoutTDD/Stack/Exercise2.py
@@ -0,0 +1,28 @@
+string = input("Enter the postfix expression with a space of separation: ")
+postfix=string
+stack = []
+
+def calculate_result(string):
+    characters = {'+': '+', '-': '-', '*': '*', '/': '/'}
+    tokens = string.split()
+    for token in tokens:
+        if token.isdigit():
+            stack.append(int(token))
+        if token in characters:
+            operator2 = stack.pop()
+            operator1 = stack.pop()
+            result = None
+            if token == '+':
+                result = operator1 + operator2
+            if token == '-':
+                result = operator1 - operator2
+            if token == '*':
+                result = operator1 * operator2
+            if token == '/':
+                result = operator1 / operator2
+            if token == '%':
+                result = operator1 % operator2
+            stack.append(result)
+    return stack.pop()
+
+print("The result of: ", postfix, "is: ", calculate_result(postfix))
